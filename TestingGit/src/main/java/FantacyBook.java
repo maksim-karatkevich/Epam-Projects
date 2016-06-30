ed9fba77fa05f7589090b2ab26e9e3f140e5f4f1
Maksim Karatkevich <Maksim_Karatkevich@epam.com>
2016-06-30 14:36:04 +0300

    11.1

---
+++ TestingGit/src/main/java/FantacyBook.java
@@ -0,0 +1,8 @@
+/**
+ * Created by maksim_kevich on 6/23/16.
+ */
+public class FantacyBook {
+    public String s = "Fantacy book";
+
+
+}
--- TestingGit/src/main/java/Mian.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by maksim_kevich on 5/13/16.
- */
-public class Mian {
-    public static void main(String[] args) {
-
-    }
-}
